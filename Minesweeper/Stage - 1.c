59eecf7a4979d7799c2609b38eb9fc1e8273ca24
kartikjain2001 <74143496+kartikjain2001@users.noreply.github.com>
2021-10-07 15:37:44 +0530

    Update Stage - 1.c

--- Minesweeper/Stage - 1.c
+++ Minesweeper/Stage - 1.c
@@ -37,3 +37,59 @@ void main()
     
 }
 
+//Updated code first and second task both
+
+#include <stdio.h>
+
+void main()
+{
+    int field[4][4] = {1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1};
+    int row , column , n_o_mines, i , j;
+    int detect_row , n_o_squares; 
+    int count = 0;
+    
+    
+    printf ("Enter the number of mines you want in the minefield: ");
+    scanf ("%d", &n_o_mines);
+    if(n_o_mines > 16)
+    {
+    printf ("Do not exceed the size of minefield.\n");
+    
+    }
+    
+    
+    else
+    {
+    printf ("Enter the coordinates where you want to place the mine starting from '0 0' for the first element and further\n");
+    for (i = 1 ; i <= n_o_mines ; i++)
+    {
+    scanf ("%d %d", &row , &column);
+    field[row][column] = 2;
+    }
+    }
+    for (row = 0 ; row <= 3 ; row++)
+    {
+        for (column = 0 ; column <= 3 ; column++)
+        {
+            printf ("%d ", field[row][column]);
+        }
+        printf ("\n");
+    }
+    printf("Enter your command in the form of four integers: ");
+    scanf ("%d %d %d %d" , &detect_row , &row , &column , &n_o_squares);
+    printf ("For the given set of commands , the number of mine sin the field is ");
+    for (j = column ; j <= column + n_o_squares - 1 ; j++)
+    {
+        
+        
+        if(field[row][j] == 2)
+        {
+            count += 1;
+        }
+        
+        
+    }
+    printf ("%d", count);
+ 
+}
+
